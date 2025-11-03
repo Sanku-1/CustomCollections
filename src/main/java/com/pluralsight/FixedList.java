90f090a62c985c408f34dbea4e9baf788d481917
Stephen_A <ankustephen99@gmail.com>
2025-11-03 11:54:45 -0500

    Created FixedList class in FixedList.java, allowing for the creation of a List of any type.

--- src/main/java/com/pluralsight/FixedList.java
+++ src/main/java/com/pluralsight/FixedList.java
@@ -1,5 +1,27 @@
 package com.pluralsight;
 
-public class FixedList<T> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        items = new ArrayList<>(maxSize);
+    }
+
+    public T add(T item){
+        if (items.size() < this.maxSize) {
+            items.add(item);
+        } else {
+            System.out.println("The List is currently full");
+        }
+        return null;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
 }
